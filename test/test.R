d3b357d1387633b78f7c6f7a8b4b58650b7161ea
Jeroen production <jeroenooms@gmail.com>
2012-04-26 16:08:13 -0700

    updated example

--- test/test.R
+++ test/test.R
@@ -5,10 +5,27 @@
 #Load library
 library(rApparmor);
 
+#test unconstrained
+read.table("/etc/passwd")
+
 #Try to change to a profile
 aa_change_profile("myprofile")
+
+#test profile
+read.table("/etc/passwd") #deny
+read.table("/etc/group") #allow
 
 #Change to a hat within the profile, and change back
 mytoken <- as.integer(123);
 aa_change_hat("myhat", mytoken);
+
+#test hat
+read.table("/etc/passwd") #deny
+read.table("/etc/group") #deny
+
+#revert back
 aa_revert_hat(mytoken);
+
+#test without hat
+read.table("/etc/passwd") #deny
+read.table("/etc/group") #allow
