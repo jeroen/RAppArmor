fb426c43284dab661bbc887cf3122af6236ef694
Jeroen <jeroen@jeroen-ubuntu.(none)>
2012-11-20 15:14:41 -0800

    added check to kill orphans

--- src/setpgid_wrapper.c
+++ src/setpgid_wrapper.c
@@ -6,12 +6,18 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <signal.h>
+#include <sys/prctl.h>
 
 void setpgid_wrapper (int *ret, int *pgid, bool *verbose) {
   if(*verbose){
 	  Rprintf("Setting pgid...\n");
   }
   *ret = setpgid (0, *pgid);
+  
+  //make sure proc doesn't orphan:
+  prctl(PR_SET_PDEATHSIG, SIGKILL);
+  
   if(*ret != 0){
     *ret = errno;
   }
