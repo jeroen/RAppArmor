4fdbd2f766f9f2aee339543b58a20e080f00ac71
Jeroen Ooms <jeroen@dev.opencpu.org>
2016-03-08 02:31:00 -0800

    Skip AppArmor tests

--- tests/testthat/helper-pretest.R
+++ tests/testthat/helper-pretest.R
@@ -1,8 +1,14 @@
-con <- aa_getcon(verbose=FALSE);
-profile <- con$con;
+skip_if_no_apparmor <- function(){
+  if (!aa_is_enabled())
+    skip("AppArmor not enabled")
+}
 
-switch(profile,
-	"/usr/bin/R" = stop("Main process must be unconfined. Please run: sudo aa-disable usr.bin.r"),
-	"unconfined" = cat("Pretest: OK\n"),
-	stop("Main process must be unconfined (", profile, ")")
-);
+if(aa_is_enabled()){
+  con <- aa_getcon(verbose=FALSE);
+  profile <- con$con;
+  switch(profile,
+     "/usr/bin/R" = stop("Main process must be unconfined. Please run: sudo aa-disable usr.bin.r"),
+     "unconfined" = cat("Pretest: OK\n"),
+     stop("Main process must be unconfined (", profile, ")")
+  );
+}
--- tests/testthat/test-apparmor.R
+++ tests/testthat/test-apparmor.R
@@ -1,6 +1,7 @@
 context("apparmor")
 
 test_that("profiles available", {
+  skip_if_no_apparmor()
 	expect_that(eval.secure(aa_getcon(verbose=FALSE)$con, profile="r-base"), equals("r-base"))
 	expect_that(eval.secure(aa_getcon(verbose=FALSE)$con, profile="r-user"), equals("r-user"))
 	expect_that(eval.secure(aa_getcon(verbose=FALSE)$con, profile="r-compile"), equals("r-compile"))
@@ -8,6 +9,7 @@ test_that("profiles available", {
 });
 
 test_that("changing hats", {
+  skip_if_no_apparmor()
 	expect_that(eval.secure({
 		aa_change_profile("testprofile", verbose=FALSE);
 		aa_change_hat("testhat", 1234, verbose=FALSE);
@@ -24,19 +26,20 @@ test_that("changing hats", {
 
 test_that("basic profiles permissions", {
 	#list /
+  skip_if_no_apparmor()
 	if(length(list.files("/")) > 0){
 		expect_that(length(eval.secure(list.files("/"))) > 0, is_true());
 		expect_that(length(eval.secure(list.files("/"), profile="r-base")) > 0, is_false());
 		expect_that(length(eval.secure(list.files("/"), profile="r-user")) > 0, is_false());
 	}
-	
+
 	#list /tmp
 	if(length(list.files("/tmp")) > 0){
 		expect_that(length(eval.secure(list.files("/tmp"))) > 0, is_true());
 		expect_that(length(eval.secure(list.files("/tmp"), profile="r-base")) > 0, is_false());
 		expect_that(length(eval.secure(list.files("/tmp"), profile="r-user")) > 0, is_false());
 	}
-	
+
 	#list /home/user/
 	if(length(list.files("~")) > 0){
 		expect_that(length(eval.secure(list.files("~"))) > 0, is_true());
