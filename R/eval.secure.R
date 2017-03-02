d7faa09c787b8613177b6784470c30986124a5f1
oliverbock <oliver@g7.org>
2017-03-03 10:02:52 +1100

    Yes, still need hack.

--- R/eval.secure.R
+++ R/eval.secure.R
@@ -179,7 +179,7 @@ eval.secure <- function(..., uid, gid, priority, profile, timeout=60,
 
 	#timeout?
 	if(is.null(myresult)){
-    if(isTRUE(timeout > 0 && totaltime >= timeout)){
+    if(isTRUE(timeout > 0 && totaltime + 0.1 >= timeout)){
 		  stop("R call did not return within ", timeout, " seconds. Terminating process.", call.=FALSE);
     } else {
       stop("R call failed: process died.", call.=FALSE);
