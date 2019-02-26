e7d98e5843e9f8b407819d16e34c30a6b4042df1
OpenCPU Server <opencpu@0d92467765c3>
2019-02-26 14:19:33 +0000

    Roxygenize

--- R/eval.R
+++ R/eval.R
@@ -1,4 +1,10 @@
+#' Sandboxing
+#' 
+#' This function has been superseded by the [unix::eval_safe] function.
+#' 
+#' @export
+#' @name sandboxing
 #' @importFrom unix eval_safe
-eval_secure <- function(...){
+eval.secure <- function(...){
   unix::eval_safe(...)
 }
